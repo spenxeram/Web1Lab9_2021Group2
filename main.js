f878ba2cc350c6d8cd999587962919463c945a30
spenxeram <spenceraustinmartin@gmail.com>
2021-03-11 13:13:15 +0700

    create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+// This script gets Bitcoin exchange data every 30 seconds from
+// https://api.coindesk.com/v1/bpi/currentprice/usd.json
+
+// global variables
+
+
+// on page load action
+
+
+// event Listeners
+
+
+// ajax call to api
+
+
+// General functions to process and output data
+
+
+// helper functions
