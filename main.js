8dd2d8e5cf033cc05655832fcf12230fb6d9c42c
spenxeram <spenceraustinmartin@gmail.com>
2021-03-11 14:17:08 +0700

    Break time updates

--- main.js
+++ main.js
@@ -8,13 +8,22 @@ let currencies = {
   vnd: 0
 };
 
-
 // on page load action
+updateCurrencies();
 document.addEventListener("DOMContentLoaded", () => {
-  let i = 0;
+  let alertbox = document.querySelector(".checking");
+  alertbox.style.opacity = "0";
+  let i = 30;
   setInterval(()=> {
-    console.log(i);
-    i++;
+    if(i == 3) {
+      alertbox.style.opacity = "1";
+    } else if (i == 0) {
+      alertbox.style.opacity = "0";
+      updateCurrencies();
+      i = 30;
+    }
+    i--;
+    outputTimer(i);
   }, 1000);
 })
 
@@ -24,9 +33,49 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 // ajax call to api
+function getBTCVal(currency) {
+  let url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`;
+  let xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    url,
+    true
+  );
+  xhr.onload = function() {
+      if(this.status == 200) {
+      let result = JSON.parse(this.responseText);
+      outputCurrency(result, currency)
+      }
+    }
 
+  xhr.send();
+}
 
 // General functions to process and output data
+function outputCurrency(result, currency) {
+  let outputTarget = document.querySelector(`.${currency}`);
+  let currencyValue = result.bpi[currency.toUpperCase()].rate_float;
+  let valueclass = "";
+  if(currencyValue >= currencies[currency]) {
+    valueclass = "green";
+  } else {
+    valueclass = "red";
+  }
+  outputTarget.classList = `${currency} ${valueclass}`;
+  currencies[currency] = currencyValue;
+  currencyValue = Math.round(currencyValue);
+  outputTarget.innerText = currencyValue.toLocaleString();
+}
 
+function updateCurrencies() {
+  Object.keys(currencies).forEach((currency) => {
+    getBTCVal(currency);
+  });
+}
+
+function outputTimer(i) {
+  let timer = document.querySelector("span.time");
+  timer.innerText = i;
+}
 
 // helper functions
