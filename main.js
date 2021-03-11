1c64fcc3b4504d3b667bbe356d6ec6039cf992d0
spenxeram <spenceraustinmartin@gmail.com>
2021-03-11 13:26:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,22 @@
 // https://api.coindesk.com/v1/bpi/currentprice/usd.json
 
 // global variables
+let currencies = {
+  usd: 0,
+  jpy: 0,
+  vnd: 0
+};
 
 
 // on page load action
+document.addEventListener("DOMContentLoaded", () => {
+  let i = 0;
+  setInterval(()=> {
+    console.log(i);
+    i++;
+  }, 1000);
+})
+
 
 
 // event Listeners
